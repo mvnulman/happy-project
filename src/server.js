34ab471de293a57ee8969a7a9110853d352a2766
Marcos <mvsouza7@gmail.com>
2020-11-04 17:43:04 +0200

    NPM

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
-console.log('oie')
+//import package
+const express = require('express')
+const path = require('path');
 
+//express initialization
+const server = express()
+server
+    //using the static files
+    .use(express.static('public'))
+    //rote create
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    })
+
+//turn on server
+server.listen(5500)
