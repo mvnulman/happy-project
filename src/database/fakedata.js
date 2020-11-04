6c6d1f2236fd1b60c8f31a383fc83ab587e6feda
Marcos <mvsouza7@gmail.com>
2020-11-04 23:19:53 +0200

    fakedata.js created

---
+++ src/database/fakedata.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 1, 
+        lat: "-27.2161717",
+        lng: "-49.6448021",
+        name: "Children's Home",
+        description: "We provide assistance to children aged 6 to 15 who are in risk situation and or social vulnerability",
+        images: [
+            "https://images.unsplash.com/photo-1604359027531-d8e4865ad050?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ]
+    },
+]
